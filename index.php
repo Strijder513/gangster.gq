dad282722a3dc840bb88fef5302d3707a7a00d55
Strijder513 <75573608+Strijder513@users.noreply.github.com>
2021-02-28 09:25:54 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,23 @@
 
+<html>
+    <link rel="stylesheet" href="/css/style.css">
+    <script type = "text/javascript" src="/js/index.js"></script>
+    <title>Welcome</title>
+<head>
+</head>
+<body>
+    
+    <!-- Makes the code a little bit cleaner, i guess.-->
+
+<div>
+    <h1 class="welcome">Welcome.</h1>
+</div>
+
+<div>
+    <a href="https://github.com/Strijder513/gangster.gq">
+    <img class="github" src="/pictures/github.png" width="100" height="100">
+</a>
+</div>
+ 
+</body>
+</html>
